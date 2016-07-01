fbcf59ec13f1fef3f28af4cf51f92c9aea21c0c7
akki2790 <akki2790@gmail.com>
2016-06-30 18:45:25 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -12,19 +12,3 @@ def hash(filename):
 
 message = hash_file("track.mp3")
 print message
-
-
-
-import hashlib
-
-def hash(filename):
-    h=hashlib.sha1()
-    with open(filename,"rb") as file:
-        chunk = 0
-        while chunk!=b"":
-            chunk=file.read(1024)
-            h.update(chunk)
-    return h.hexdigest()
-
-message = hash("track.mp3")
-print message
