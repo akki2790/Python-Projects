70d889f8226d87a14296fa5e343a64abc204b189
akki2790 <akki2790@gmail.com>
2016-07-26 18:05:53 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,37 +1,16 @@
-file = open (".txt")
 
-name_list=[]
-for line in file:
-    line=line.strip()
-    name_list.append(line)
-
-file.close()
-
-key = "Some shit"
-
-i = 0
-while i < len(name_list) and name_list[i]!=key:
-    i+=1
-
-if i<len(name_list):
-    print "The name is at the position ", i
-else:
-    print "The name is not in the list."
-
-
-#***************
 
 
 file = open("something.txt")
 
 name_list1=[]
 for line in file:
-    line=line.strip()
-    name_list.append(line)
+    name=line.split()
+    name_list.append(name)
 
 file.close()
 
-key = "some shit"
+key = "something"
 
 i=0
 while i < len(name_list) and name_list[i]!=key:
