0266d3bfa169a3e17c9c62e59853a364c4bf0e6a
akki2790 <akki2790@gmail.com>
2016-11-10 22:22:27 -0800

    correction

--- double.py
+++ double.py
@@ -8,17 +8,3 @@ def double(array):
             return True
 
 double("abhsjeufbskncvleyhdks")
-
-
-def double1(array1):
-    checklist1=[]
-    for a in array1:
-        a=a.lower()
-        if a not in checklist1:
-            checklist1.append(a)
-            print "".join(checklist1)
-        else:
-            print a+" double hai bhai"
-            return True
-
-double1("sdbJKSBUIBKLSJDbvusbKCV DS")
